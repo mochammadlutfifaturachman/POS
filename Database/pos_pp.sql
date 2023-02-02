481e61ff859db4fefa99c2b3f2057f6c6da23214
fioraaaa <78577110+fioraaaa@users.noreply.github.com>
2023-02-02 19:18:02 +0700

    add data dummy

--- Database/pos_pp.sql
+++ Database/pos_pp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2023 at 04:23 AM
+-- Generation Time: Feb 02, 2023 at 01:16 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -34,6 +34,15 @@ CREATE TABLE `kategori` (
   `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `nama_kategori`, `date_created`, `date_modified`) VALUES
+(1, 'Noodle', '2023-02-02 06:26:28', '2023-02-02 05:26:40'),
+(2, 'Snack', '2023-02-02 06:27:46', '2023-02-02 05:27:52'),
+(3, 'Ice Cream', '2023-02-02 06:28:06', '2023-02-02 05:28:15');
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +55,16 @@ CREATE TABLE `member` (
   `no_whatsapp` varchar(15) NOT NULL,
   `email_member` varchar(100) NOT NULL,
   `status_member` enum('Y','N') NOT NULL,
-  `no_member` int(7) NOT NULL
+  `no_member` varchar(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nama_member`, `no_whatsapp`, `email_member`, `status_member`, `no_member`) VALUES
+(1, 'Richo', '08974633211', 'richo@gmail.com', 'Y', '0223001'),
+(2, 'Berliana', '08287655499', 'berliana@gmail.com', 'Y', '0223002');
 
 -- --------------------------------------------------------
 
@@ -64,6 +81,17 @@ CREATE TABLE `pembayaran` (
   `status_bayar` enum('Y','N') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_user`, `total_pembayaran`, `total_diskon`, `no_pembayaran`, `status_bayar`) VALUES
+(1, 1, '56000', '5600', '0202231244001', 'Y'),
+(2, 3, '45000', '4500', '0202231246002', 'N'),
+(3, 3, '99999', '9999', '0202231248003', 'Y'),
+(4, 2, '42900', '4290', '0202231250004', 'N'),
+(5, 1, '43000', '4300', '0202231250005', 'Y');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +101,7 @@ CREATE TABLE `pembayaran` (
 CREATE TABLE `produk` (
   `produk_id` int(5) NOT NULL,
   `kode_produk` varchar(25) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
   `kategori_id` int(5) NOT NULL,
   `harga_modal` varchar(50) NOT NULL,
   `harga_jual` varchar(50) NOT NULL,
@@ -82,6 +111,17 @@ CREATE TABLE `produk` (
   `date_created` datetime NOT NULL,
   `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`produk_id`, `kode_produk`, `nama_produk`, `kategori_id`, `harga_modal`, `harga_jual`, `stok`, `gambar`, `expired_date`, `date_created`, `date_modified`) VALUES
+(1, '89686010947', 'Indomie Goreng Spesial', 1, '2300', '2700', 1000, 'IndomieGorengSpesial.png', '2024-02-29', '2023-02-02 07:01:43', '2023-02-02 06:11:11'),
+(2, '8992112206001', 'Indomie Soto', 1, '2200', '2600', 995, 'IndomieSoto.png', '2023-02-21', '2023-02-02 07:11:17', '2023-02-02 06:12:42'),
+(3, '8992981206023', 'Tango Wafer Chocolate', 2, '8888', '10000', 388, 'TangoWaferChocolate.png', '2025-04-19', '2023-02-02 07:12:53', '2023-02-02 06:16:26'),
+(4, '8992928206231', 'Oreo Ice Cream', 2, '8250', '9000', 558, 'OreoIceCream.png', '2025-05-29', '2023-02-02 07:16:30', '2023-02-02 06:18:38'),
+(5, '8999482206023', 'Cornetto Oreo', 3, '11400', '12000', 500, 'CornettoOreo.png', '2025-06-19', '2023-02-02 07:18:45', '2023-02-02 06:21:09');
 
 -- --------------------------------------------------------
 
@@ -108,6 +148,17 @@ CREATE TABLE `transaksi` (
   `jml_pesan` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_produk`, `id_pembayaran`, `jml_pesan`) VALUES
+(1, 1, 1, 5),
+(2, 2, 2, 7),
+(3, 3, 3, 5),
+(4, 1, 4, 3),
+(5, 3, 5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -121,6 +172,14 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `role` enum('Admin','Kasir') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama_user`, `email_user`, `password`, `role`) VALUES
+(1, 'Fiora', 'fiora@gmail.com', 'fiora', 'Admin'),
+(2, 'Putri', 'putri@gmail.com', 'putri', 'Kasir');
 
 --
 -- Indexes for dumped tables
@@ -176,25 +235,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `kategori_id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `kategori_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id_member` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_member` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pembayaran` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `produk_id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `produk_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `settings`
@@ -206,13 +265,13 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
